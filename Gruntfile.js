d63d615e0856ff322399767b2e846406eea91e93
Jeronimo Lopez <jerolba@MacBook-Pro-de-Jeronimo.local>
2017-03-21 10:50:30 +0100

    Disable grunt image optimization… error on npm install

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,15 +67,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-svgmin');
+  //grunt.loadNpmTasks('grunt-contrib-imagemin');
+  //grunt.loadNpmTasks('grunt-svgmin');
 
   // Register tasks
   grunt.registerTask('default', [
     'clean',
     'uglify',
-    'imagemin',
-    'svgmin'
+    //'imagemin',
+    //'svgmin'
   ]);
   grunt.registerTask('dev', [
     'watch'
